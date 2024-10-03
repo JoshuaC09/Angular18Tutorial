8145e6d38c1a947fdd93dc60adf6d67029c34467
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-10-03 14:58:09 +0800

    Added ppt for angular 18 course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
