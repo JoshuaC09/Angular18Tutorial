bc74cb1a968f950188865d74ae9dbbff1a8441eb
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-10-03 17:23:00 +0800

    Added Pipes and Property binding and interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from './custom/reverse.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, HomeComponent],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    HomeComponent,
+    CommonModule,
+    ReversePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'newstandalone';
+  title = 'Angular 18 Tutorial';
+  subtitle = 'Angular for beginners';
+  todaydate = new Date();
+  salary = 1000000;
+  _obj = { name: 'Nt' };
+
+  isDisabled = false;
 }
