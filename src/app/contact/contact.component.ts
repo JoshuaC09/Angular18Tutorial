7c2b8151a861b11b7afa53e8cfb462b429f12024
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-10-04 14:33:02 +0800

    Done with angular hooks

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
   imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
 })
-export class ContactComponent {
-
+export class ContactComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked
+{
+  ngAfterContentChecked(): void {
+    console.log('Test angular hook ngAferContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('Test angular hooks ngAfterContentInit');
+  }
+  ngAfterViewInit(): void {
+    console.log('Test angular hooks ngAfterViewInit');
+  }
+  ngAfterViewChecked(): void {
+    console.log('Test angular hooks ngAfterViewChecked');
+  }
+  ngDoCheck(): void {
+    console.log('Test angular hooks ngDoCheck');
+  }
+  ngOnInit(): void {
+    console.log('Test angular hooks ngOnInit');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('Test angular hooks ngOnChanges');
+  }
 }
