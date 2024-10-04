5ae32ed5c022b36a975176e95539974408468e6d
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-10-04 10:08:20 +0800

    Added route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutsComponent } from './common/abouts/abouts.component';
+import { ContactComponent } from './contact/contact.component';
+import { CustomerComponent } from './common/customer/customer.component';
+import { AddComponent } from './common/add/add.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'about',
+    component: AboutsComponent,
+  },
+  {
+    path: 'about/:submenu/:id',
+    component: AboutsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
+  {
+    path: 'customer',
+    component: CustomerComponent,
+    children: [
+      { path: 'add', component: AddComponent },
+      { path: 'edit/:id', component: AddComponent },
+    ],
+  },
+];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { ReversePipe } from '../custom/reverse.pipe';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, CommonModule, ReversePipe, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  title = 'Angular 18 Tutorial';
+  subtitle = 'Angular for beginners';
+  todaydate = new Date();
+  salary = 1000000;
+  _obj = { name: 'Nt' };
+
+  isDisabled = false;
+
+  ChangeTitle() {
+    this.title = 'angular 18 full turial';
+  }
+  UpdateTitle(event: any) {
+    this.title = event.target.value;
+  }
+}
