9e0f417f967443c9822bce45fb70b6f8a2ff23f8
JoshuaC09 <117239417+JoshuaC09@users.noreply.github.com>
2024-10-04 10:17:46 +0800

    Done with route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutsComponent } from './common/abouts/abouts.component';
 import { ContactComponent } from './contact/contact.component';
 import { CustomerComponent } from './common/customer/customer.component';
 import { AddComponent } from './common/add/add.component';
+import { StatusComponent } from './common/status/status.component';
 
 export const routes: Routes = [
   {
@@ -29,5 +30,9 @@ export const routes: Routes = [
       { path: 'add', component: AddComponent },
       { path: 'edit/:id', component: AddComponent },
     ],
+  },
+  {
+    path: '**',
+    component: StatusComponent,
   },
 ];
